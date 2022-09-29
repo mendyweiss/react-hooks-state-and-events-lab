be450862381bc6c565efa66fc86516428db5813d
Mendy <mendyw94@gmail.com>
2022-09-29 10:00:59 -0400

    Completed Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React , {useState} from "react";
+
 
 function Item({ name, category }) {
+
+  const [added, setAdded] = useState(false)
+
+  function displayButtonFunction () {
+    return added ? 'Remove from Cart' : 'Add to Cart'
+  }
+
+  function classNameAttacher () {
+    if (added) {
+      return "remove in-cart"
+    } else {
+      return "add"
+    }
+  }
+
   return (
-    <li className="">
+    <li className={added ? "in-cart" : " "}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={classNameAttacher()} onClick={() => setAdded(!added)}>{displayButtonFunction()}</button>
     </li>
   );
 }
